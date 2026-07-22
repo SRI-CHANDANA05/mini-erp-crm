258e0933849ad2a846b24b622074d9a5bc42ddbd
SRI-CHANDANA05 <ksrichandana2005@gmail.com>
2026-07-22 20:13:09 +0530

    Add Stock class for managing stock data

---
+++ backend/models/Stock.js
@@ -0,0 +1,9 @@
+class Stock {
+    constructor(id, productId, quantity) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+}
+
+module.exports = Stock;
